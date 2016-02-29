e63fa06ab6c48c3be5bcd5831015f582c3f86f66
Dan Galipo <dan.v.galipo@gmail.com>
2016-02-29 20:00:35 +1100

    implemented robot#placed

--- lib/robot.rb
+++ lib/robot.rb
@@ -2,6 +2,7 @@ class Robot
 
   attr_accessor :x_position, :y_position, :direction,
     :placed
+  attr_reader :coordinates
 
   def initialize
     self.placed = false
@@ -10,5 +11,15 @@ class Robot
   def placed?
     placed
   end
+
+  def place(coordinates, direction)
+    self.coordinates = coordinates
+    self.direction = direction
+    self.placed = true
+  end
+
+ private
+
+ attr_writer :coordinates
 
 end

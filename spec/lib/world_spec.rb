83162215e277022f68f21f3059f378936c670a43
Dan Galipo <dan.v.galipo@gmail.com>
2016-03-01 17:40:22 +1100

    Updated World specs to no longer delegate to singleton instance

--- spec/lib/world_spec.rb
+++ spec/lib/world_spec.rb
@@ -1,34 +1,10 @@
 require "spec_helper"
 
 describe World do
-
-  describe ".instance" do
-    subject(:instance) { World.instance }
-
-    specify { expect(instance.x_plane).to eq((0..4)) }
-    specify { expect(instance.y_plane).to eq((0..4)) }
-    specify { expect(instance.directions.size).to eq(4) }
-    specify do
-      expect(instance.directions.first)
-        .to eq({name: "NORTH", move_x: 0, move_y: 1})
-    end
-    specify do
-      expect(instance.directions[1])
-        .to eq({name: "EAST", move_x: -1, move_y: 0})
-    end
-    specify do
-      expect(instance.directions[2])
-        .to eq({name: "SOUTH", move_x: 0, move_y: -1})
-    end
-    specify do
-      expect(instance.directions.last)
-        .to eq({name: "WEST", move_x: 1, move_y: 0})
-    end
-
-  end
+  let(:world) { World.new }
 
   describe ".direction_names" do
-    subject(:direction_names) { World.direction_names }
+    subject(:direction_names) { world.direction_names }
 
     specify { expect(direction_names.size).to eq(4) }
     specify { expect(direction_names.first).to eq("NORTH") }
@@ -38,19 +14,19 @@ describe World do
   end
 
   describe ".x_move_for_direction" do
-    subject { World.x_move_for_direction(0) }
+    subject { world.x_move_for_direction(0) }
 
     it { is_expected.to eq(0) }
   end
 
   describe ".y_move_for_direction" do
-    subject { World.y_move_for_direction(0) }
+    subject { world.y_move_for_direction(0) }
 
     it { is_expected.to eq(1) }
   end
 
   describe ".on_x_plane?" do
-    subject { World.on_x_plane?(position) }
+    subject { world.on_x_plane?(position) }
 
     context "position is on x plane" do
       let(:position) { 1 }
@@ -66,7 +42,7 @@ describe World do
   end
 
   describe ".on_y_plane?" do
-    subject { World.on_y_plane?(position) }
+    subject { world.on_y_plane?(position) }
 
     context "position is on y plane" do
       let(:position) { 1 }
@@ -82,13 +58,13 @@ describe World do
   end
 
   describe ".direction_left_of_direction" do
-    subject { World.direction_left_of_direction(0) }
+    subject { world.direction_left_of_direction(0) }
 
     it { is_expected.to eq(3) }
   end
 
   describe ".direction_right_of_direction" do
-    subject { World.direction_right_of_direction(3) }
+    subject { world.direction_right_of_direction(3) }
 
     it { is_expected.to eq(0) }
   end

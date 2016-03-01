18cd5c787def0c26db7596a6db06bb2485bbd137
Dan Galipo <dan.v.galipo@gmail.com>
2016-03-01 17:46:26 +1100

    update world to use direction objects and no longer be a singleton

--- lib/world.rb
+++ lib/world.rb
@@ -1,38 +1,27 @@
 require "singleton"
 require "forwardable"
 class World
-  include Singleton
-
-  attr_accessor :x_plane, :y_plane, :directions
-
   def initialize
     self.x_plane = (0..4)
     self.y_plane = (0..4)
     self.directions = [
-      { name: "NORTH", move_x: 0, move_y: 1 },
-      { name: "EAST", move_x: -1, move_y: 0 },
-      { name: "SOUTH", move_x: 0, move_y: -1 },
-      { name: "WEST", move_x: 1, move_y: 0 },
+      Direction.new(name: "NORTH", move_x: 0, move_y: 1),
+      Direction.new(name: "EAST", move_x: -1, move_y: 0),
+      Direction.new(name: "SOUTH", move_x: 0, move_y: -1),
+      Direction.new(name: "WEST", move_x: 1, move_y: 0),
     ]
   end
 
-  class << self
-    extend Forwardable
-    def_delegators :instance, :direction_names, :x_move_for_direction,
-      :y_move_for_direction, :on_x_plane?, :on_y_plane?,
-      :direction_left_of_direction, :direction_right_of_direction
-  end
-
   def direction_names
-    directions.map{|direction| direction[:name]}
+    directions.map{|direction| direction.name }
   end
 
   def x_move_for_direction(direction)
-    directions[direction][:move_x]
+    directions[direction].move_x
   end
 
   def y_move_for_direction(direction)
-    directions[direction][:move_y]
+    directions[direction].move_y
   end
 
   def on_x_plane?(position)
@@ -51,4 +40,7 @@ class World
     (direction + 1) % directions.size
   end
 
+  private
+
+  attr_accessor :x_plane, :y_plane, :directions
 end

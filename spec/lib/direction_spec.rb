7e7508510106c710be9aa41dfe47912908a1dc53
Dan Galipo <dan.v.galipo@gmail.com>
2016-03-01 17:21:29 +1100

    Added Basic Direction spec

---
+++ spec/lib/direction_spec.rb
@@ -0,0 +1,31 @@
+require 'spec_helper'
+
+describe Direction do
+  let(:direction) do
+    Direction.new(name: name,
+                  move_x: move_x,
+                  move_y: move_y)
+  end
+  let(:name)    { "WEST" }
+  let(:move_x)  { 1 }
+  let(:move_y)  { 0 }
+
+  describe '#name' do
+    subject { direction.name }
+
+    it { is_expected.to eq(name) }
+  end
+
+  describe '#move_x' do
+    subject { direction.move_x }
+
+    it { is_expected.to eq(move_x) }
+  end
+
+  describe '#move_y' do
+    subject { direction.move_y }
+
+    it { is_expected.to eq(move_y) }
+  end
+
+end
